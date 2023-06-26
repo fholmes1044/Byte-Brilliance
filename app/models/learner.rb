c17983e2280d772931a19dff0f18c88868f2582c
+ <+>
2023-06-26 16:28:32 -0400

    added validations for learner model

--- app/models/learner.rb
+++ app/models/learner.rb
@@ -1,5 +1,15 @@
 class Learner < ApplicationRecord
+    has_secure_password
+
     has_many :posts 
     has_many :learner_to_tutors
     has_many :tutors, through: :learner_to_tutors
+
+    validates :username, :full_name, :age, :email, :password, :password_confirmation, :learning_goals, :location,  presence: true
+    validates :age, numericality: {greater_than_or_equal_to: 18}
+    validates :username, length: { minimum: 3, maximum: 50 }
+    validates :password_digest, length: { minimum: 8 }
+    validates :email, uniqueness: { case_sensitive: true }
+    validates :username, uniqueness: { case_sensitive: false }
+    validates :password, format: { with: /\A(?=.*[!@#\$%^&*()\-_=+{}\[\]|\\:;"'<>,.?\/])(?=.*[0-9]).{8,}\z/}
 end
---
+++ db/schema.rb
@@ -0,0 +1,76 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.0].define(version: 2023_06_26_025358) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "learner_posts", force: :cascade do |t|
+    t.integer "learner_id"
+    t.string "summary"
+    t.string "date"
+    t.string "string"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "learner_to_tutors", force: :cascade do |t|
+    t.integer "tutor_id"
+    t.integer "learner_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "learners", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
+    t.string "email"
+    t.string "profile_picture"
+    t.string "full_name"
+    t.string "learning_goals"
+    t.integer "age"
+    t.string "location"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "meetings", force: :cascade do |t|
+    t.integer "learner_to_tutor_id"
+    t.date "date"
+    t.integer "duration"
+    t.string "location"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tutor_reviews", force: :cascade do |t|
+    t.integer "learner_to_tutor_id"
+    t.string "rating"
+    t.string "integer"
+    t.string "review_summary"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tutors", force: :cascade do |t|
+    t.string "name"
+    t.string "experience"
+    t.integer "hourly_rate"
+    t.string "availability"
+    t.string "location"
+    t.string "subject"
+    t.integer "age"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
